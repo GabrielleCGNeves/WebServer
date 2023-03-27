044ffd6886efdefbb4c09bb6b0f16f1f02c563f1
unknown <gabriellecarvalhoneves@gmail.com>
2023-03-27 16:04:24 -0300

    :sparkles: created routes to each json file

--- helloHTTP.js
+++ helloHTTP.js
@@ -1,14 +1,38 @@
 
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
+
+function readFile(response, file) {
+    fs.readFile(file, (err, data) => {
+        response.end(data)
+    })
+}
 
 var callback = function(request, response){
-    response.writeHead(200,{"Content-tipe": "text/plain; charset=utf-8"});
+    response.writeHead(200,{"Content-tipe": "application/json"});
     
     var part  = url.parse(request.url);
+
     if(part.path == "/"){
         response.end("Site Principal");
     }
+    
+    else if(part.path == "/rota1/cadastro"){
+        readFile(response, "cadastro.json");
+    }
+
+    else if(part.path == "/rota1/catalogo"){
+        readFile(response, "catalogo.json");
+    }
+
+    else if(part.path == "/rota1/dados"){
+        readFile(response, "dados.json");
+    }        
+
+    else if(part.path == "/rota1/site"){
+        readFile(response, "index.html");
+    }        
     
     else if(part.path == "/rota1"){
         response.end("Site do rota1");
